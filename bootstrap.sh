eb46b5bad19f5324ce1b9edd564efa2d2cd911e7
Ryan Spaulding <ryanspaulding@gmail.com>
2014-03-26 12:34:55 -0700

    adding in the yum update and getting stuff ready for chef

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,25 @@
+#!/bin/bash
+
+LOGFILE="/root/bootstrap.log"
+
+function log {
+        echo "[`date`] ${1}" | tee -a ${LOGFILE}
+}
+
+if [ ! -f "${LOGFILE}" ]
+then
+        touch ${LOGFILE} 
+fi
+
 # yum update 
+log "Updating System"
+yum check-update | tee -a ${LOGFILE}
 
-# install git 
+log "Installing packages needed for Chef"
+yum install -y git ruby ruby-devel rubygems rubygems-devel gcc | tee -a ${LOGFILE}
 
-
-# install chef-solo 
+log "Installing Chef"
+gem install chef | tee -a ${LOGFILE}
 
 # git checkout chef crap 
 
