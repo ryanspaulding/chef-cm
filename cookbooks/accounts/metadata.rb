22daf7145ca3e37291cf55f48eee0abd84a49cda
Ryan Spaulding <ryanspaulding@gmail.com>
2014-03-27 12:58:27 -0700

    adding first cut of the two cookbooks

---
+++ cookbooks/accounts/metadata.rb
@@ -0,0 +1,6 @@
+name         "accounts"
+maintainer       "Ryan Spaulding"
+maintainer_email "ryan@climatemonkeys.com"
+description      "Setup up developers accounts"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ cookbooks/accounts/recipes/default.rb
@@ -0,0 +1,8 @@
+# this is a place holder. Need too fingure out if we leave
+# the password option off what it does when it creates the account
+
+user "systemguy" do
+  comment "system guy"
+  system true
+  shell "/bin/false"
+end
