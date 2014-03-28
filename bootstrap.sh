332797b7ee8ade1af83d1473c2b18453d2195347
Ryan Spaulding <ryanspaulding@gmail.com>
2014-03-28 08:32:16 -0700

    fixing issues with installing and using software collections

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 LOGFILE="/root/bootstrap.log"
 CHEFCONFIG="/etc/chef/"
 CHEFFILES="/var/chef-solo/"
-GITURL="git@github.com:ryanspaulding/chef.git"
+GITURL="git@github.com:ryanspaulding/chef-cm.git"
 
 function log {
         echo "[`date`] ${1}" | tee -a ${LOGFILE}
@@ -26,13 +26,19 @@ yum install -y git ruby ruby-devel rubygems rubygems-devel gcc | tee -a ${LOGFIL
 log "Installing software collections"
 yum install -y scl-utils scl-utils-build | tee -a ${LOGFILE}
 
-log "Installing Ruby 1.9.2"
-yum install -y ruby1.9.2 # TODO get correct package name
+if [ ! -f "/etc/yum.repos.d/scl_ruby193.repo" ]
+then
+	cd /etc/yum.repos.d/
+	wget http://people.redhat.com/bkabrda/scl_ruby193.repo
+fi
+
+log "Installing Ruby 1.9.3"
+yum install -y ruby193 
 
 log "Installing Chef"
-slc enaable ruby1.9.2 'gem install chef' | tee -a ${LOGFILE} # TODO get correct package name
+scl enable ruby193 'gem install chef' | tee -a ${LOGFILE} 
 
-# git checkout chef crap 
+log "Getting Chef files from GitHub"
 if [ ! -d "${CHEFFILES}" ]
 then
     git clone ${GITURL} ${CHEFFILES} 
@@ -41,6 +47,7 @@ else
 fi
 
 # build chef-solo file 
+log "Setting up Chef solo config"
 if [ ! -d "${CHEFCONFIG}" ]
 then
     mkdir -p "${CHEFCONFIG}"
@@ -58,12 +65,12 @@ cache_options({ :path => "${CHEFCONFIG}cache/checksums", :skip_expires => true }
 SOLO
 fi
 
-# build runlist file 
+log "Creating runlist.json"
 if [ ! -f "${CHEFFILES}runlist.json" ]
 then
 cat << 'RUN' > "${CHEFFILES}runlist.json"
 { "run_list": "role[devbox]" }
 RUN
 
-# run chef 
-chef-solo -j ${CHEFFILES}runlist.json
+log "Cheffing!!!!!"
+#chef-solo -j ${CHEFFILES}runlist.json
