c7d3aaa874cf1b6263d790ba265da5848581f865
Ryan Spaulding <ryanspaulding@gmail.com>
2014-03-29 12:01:00 -0700

    adding a common group for devs to be added to

--- cookbooks/accounts/recipes/default.rb
+++ cookbooks/accounts/recipes/default.rb
@@ -1,6 +1,11 @@
 # this is a place holder. Need too fingure out if we leave
 # the password option off what it does when it creates the account
 
+group "dev" do
+    action :create
+    system false
+end
+
 user "systemguy" do
   comment "system guy"
   system true
